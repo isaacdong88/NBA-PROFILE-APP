97b5710112fb6f4d7ba57d8595a99c4cca30ef2a
isaacdong88 <isaacdong88@gmail.com>
2023-03-20 13:05:07 -0400

    add state to save playerID to use in second fetch url

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import noImg from '../noProfileImg.png'
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 function Player(props) {
+    const [playerId, setPlayerId] = useState(null)
     const searchTerm = props.player.lastName
     const getPlayer = async () => {
         const response = await fetch(`https://www.balldontlie.io/api/v1/players?search=${searchTerm}&per_page=100`);
@@ -10,19 +11,23 @@ function Player(props) {
         const player = data.data.filter((player) => player.first_name === props.player.firstName)[0]
         // console.log(data)
         console.log(player);
-        // const playerID = player.id
-        // console.log(playerID)
-        // const getStats = async () => {
-        //     const response2 = await fetch(`https://www.balldontlie.io/api/v1/season_averages?season=2022&player_ids[]=${playerID}`)
-        //     const data2 = await response2.json()
-        //     const stats = data2.data.
-
-        // }
+        const playerID = player.id
+        setPlayerId(playerID)
       };
+      console.log(playerId)
 
       useEffect(() => {
         getPlayer();
       }, []);
+
+    // const getStats = async () => {
+    //     const response2 = await fetch(`https://www.balldontlie.io/api/v1/season_averages?season=2022&player_ids[]=${playerID}`)
+    //     const data2 = await response2.json()
+    //     const stats = data2.fgm
+    //     console.log(stats)
+    // }
+    // getStats()
+
   return (
     <div>
         <img className='playerImg' src={`http://cdn.nba.com/headshots/nba/latest/260x190/${props.player.personId}.png`} alt="" onError={event => {event.target.src = noImg}} />
