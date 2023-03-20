62c2df425f79279cb3f714bc3bdd1ef4db9b2d93
isaacdong88 <isaacdong88@gmail.com>
2023-03-20 01:17:24 -0400

    add fetch function in player component search using player id key from player data to match API

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Player from "./components/Player";
 
 function App() {
   const [display, setDisplay] = useState(null);
+  const [stats, setStats] = useState(null);
 
   const fetchPlayers = (byTeam) => {
     setDisplay(
@@ -35,11 +36,15 @@ function App() {
   }, []);
 
   // const fetchTeam = async () => {
-  //   const response = await fetch("players");
+  //   const response = await fetch("https://www.balldontlie.io/api/v1/players");
   //   const data = await response.json();
-  //   setTeam(data);
-  //   console.log(team);
+  //   setStats(data);
+  //   console.log(data);
   // };
+
+  // useEffect(() => {
+  //   fetchTeam();
+  // }, []);
   // fetchTeam();
   // useEffect(() => {
   //   fetchTeam(), [];
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
 import noImg from '../noProfileImg.png'
+import { useEffect } from 'react';
 
 function Player(props) {
+    const searchTerm = props.player.lastName
+    const fetchTeam = async () => {
+        const response = await fetch(`https://www.balldontlie.io/api/v1/players?search=${searchTerm}`);
+        const data = await response.json();
+        const player = data.data.filter((player) => player.first_name === props.player.firstName)[0]
+        console.log(data)
+        console.log(player);
+      };
+
+      useEffect(() => {
+        fetchTeam();
+      }, []);
   return (
     <div>
         <img className='playerImg' src={`http://cdn.nba.com/headshots/nba/latest/260x190/${props.player.personId}.png`} alt="" onError={event => {event.target.src = noImg}} />
