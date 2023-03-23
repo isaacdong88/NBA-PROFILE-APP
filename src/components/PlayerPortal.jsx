29c2f4b7368a5a77b3bcbd6e878e6d67aa1a9266
isaacdong88 <isaacdong88@gmail.com>
2023-03-23 15:25:19 -0400

    add containers for stats in modal

--- src/components/PlayerPortal.jsx
+++ src/components/PlayerPortal.jsx
@@ -10,7 +10,7 @@ function PlayerPortal(props) {
         e.stopPropagation();
       }} className='modalContainer'>
         <div className='close-btn'>
-          <button onClick={props.onclose}>X</button>
+          {/* <button onClick={props.onclose}>X</button> */}
         </div>
         <div className='imgModalContainer'>
           <img className='playerImgModal' src={`http://cdn.nba.com/headshots/nba/latest/260x190/${props.personId}.png`} alt="" onError={event => {event.target.src = noImg}} />
@@ -18,10 +18,15 @@ function PlayerPortal(props) {
         <div className='playerName'>
           {props.playerName.firstName} {props.playerName.lastName}
         </div>
-        <div>
-          {props.stats[0]?.fga ? props.stats[0].fga : "N/A"} <br />
-          {props.stats[0]?.fgm ? props.stats[0].fgm : "N/A"} 
-
+        <div className='stats-ctn'>
+          <div>Season <br />{props.stats[0]?.season ? props.stats[0].season : "N/A"}</div>
+          <div>Games Played <br />{props.stats[0]?.fgm ? props.stats[0].fgm : "N/A"} </div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
         </div>
       </div>
    </div> 
